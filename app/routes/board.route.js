1ebda81e5a852f2613aef5b548915e5e46fa5615
seoseongmin96 <justin090452@gmail.com>
2022-04-08 17:52:49 +0900

    2022-04-08 express boardlist complete

--- app/routes/board.route.js
+++ app/routes/board.route.js
@@ -1,2 +1,3 @@
-const { write } = require('../controllers/board.controller');
-module.exports = x => x.app.post(`${x.url}/write`, write) ;
+const { boardform, boardlist } = require('../controllers/board.controller');
+module.exports = x => {x.app.post(`${x.url}/board-form`, boardform)
+x.app.get(`${x.url}/list`, boardlist)};
