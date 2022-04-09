8f8679f7cbf24791c6f32ca7f767c40d65ebe5a9
seoseongmin96 <justin090452@gmail.com>
2022-04-09 16:56:57 +0900

    2022-04-09 express userlist link

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,3 +1,4 @@
-const { signup, userlist } = require('../controllers/user.controller');
-module.exports = x => x.app.post(`${x.url}/sign-up`, signup);
-module.exports = x => x.app.get(`${x.url}/list`, userlist) ;
+const { signup, userlist, profile } = require('../controllers/user.controller');
+module.exports = x => {x.app.post(`${x.url}/sign-up`, signup)
+x.app.get(`${x.url}/list`, userlist)
+x.app.get(`${x.url}/profile/:id`, profile)};
