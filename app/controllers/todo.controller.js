dccf94487bb8b1b0012184ffff8e866747cc30c8
seoseongmin96 <justin090452@gmail.com>
2022-04-19 11:46:44 +0900

    2022-04-19 express crud training 1

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -1,10 +1,25 @@
-exports.write = (req,res) => {
-    const {passengerId, name, teamId, subject} = req.body
-    console.log(`넘어온 JSON 값 : ${JSON.stringify(req.body)}`)
-    console.log(`passengerId 값 : ${passengerId}`)
-    console.log(`name 값 : ${name}`)
-    console.log(`teamId 값 : ${teamId}`)
-    console.log(`subject 값 : ${subject}`)
-    res.status(200).json({'reuslt' : 'ok'})
-
+const db = require('../models/index')
+const todoSchema = db.todo
+exports.addTodo = (req, res) => {
+  console.log(' ### 진행 4: 노드서버에 진입함 '+ JSON.stringify(req.body))
+  new todoSchema(req.body).save(()=>{
+    res.status(200).json({'result':'OK'})
+  })
+}
+exports.getTodo = (req, res) => {
+  console.log(`### todoController access ### `)
+  todoSchema.find()
+  .exec((err, todos) => {
+    if (err) return res.status(400).send(err)
+    res.status(200).json({ success : true, todos})
+    console.log(todos)
+  })
+}
+exports.profile = (req, res) => {
+  console.log(`### user profile access ### `)
+  todoSchema.find({task: req.params.id})
+  .exec((err, todo) => {
+      if (err) return res.status(400).send(err)
+      res.status(200).json({ success: true, todo })
+  })
 }
